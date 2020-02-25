294696637605eaa1578a641748aa6135bc0c7dcb
conaloneill <conaloneill@users.noreply.github.com>
2020-02-25 14:00:55 +0000

    Vapor 4 updates to Package.swift

--- Package.swift
+++ Package.swift
@@ -2,23 +2,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "MailCore",
+    name: MailCore,
     platforms: [
        .macOS(.v10_14)
     ],
     products: [
-        .library(name: "MailCore", targets: ["MailCore"]),
+        .library(name: MailCore, targets: [MailCore]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
-        .package(url: "https://github.com/skelpo/sendgrid-provider.git", from: "4.0.0-beta"),
+        .package(url: https://github.com/vapor/vapor.git, from: 4.0.0-beta),
+        .package(url: https://github.com/skelpo/sendgrid-provider.git, from: 4.0.0-beta),
     ],
     targets: [
-        .target(name: "MailCore", dependencies: [
-            "Vapor",
-            "SendGrid"
+        .target(name: MailCore, dependencies: [
+            Vapor,
+            SendGrid
         ]),
-        .target(name: "Run", dependencies: ["MailCore"]),
-        .testTarget(name: "AppTests", dependencies: ["MailCore", "XCTVapor"])
+        .target(name: Run, dependencies: [MailCore]),
+        .testTarget(name: AppTests, dependencies: [MailCore, XCTVapor])
     ]
 )
+
